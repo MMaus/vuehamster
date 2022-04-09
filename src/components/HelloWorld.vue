2fb779b11286e231777abc4be186a49ff4f17cd9
Moritz Maus <h.moritz.maus@gmail.com>
2022-04-09 10:42:20 +0200

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string;
+}>();
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>This is the vue3 scaffold, going to become a timetracker app.</h3>
+    For now it's a playground for
+    <ul>
+      <li>Exploring github actions</li>
+      <li>Exploring tailwind css / headless ui</li>
+      <li>Exploring vite / vitest</li>
+      <li>Exploring scss etc. in vue builds</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+li {
+  margin-left: 1em;
+  font-size: smaller;
+}
+
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <span>Simple test cases</span>
+    <div class="p-4 bg-purple-600 text-gray-100 rounded m-2">
+      <Menu>
+        <MenuButton>Menu </MenuButton>
+        <MenuItems>
+          <MenuItem v-slot="{ active }">
+            <RouterLink
+              to="/"
+              :class="{ 'bg-blue-500': active }"
+              class="font-bold"
+              >Tracking</RouterLink
+            >
+          </MenuItem>
+          <MenuItem v-slot="{ active }">
+            <RouterLink to="/" :class="{ 'bg-blue-500': active }"
+              >Overview</RouterLink
+            >
+          </MenuItem>
+        </MenuItems>
+      </Menu>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { Menu, MenuItems, MenuItem, MenuButton } from "@headlessui/vue";
+import { RouterLink } from "vue-router";
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    padding: 5em;
+  }
+}
+</style>
